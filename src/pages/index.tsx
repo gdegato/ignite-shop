3c630208c1748acb24b243c61431af8d533ead20
gdegato <rosele.tst@gmail.com>
2023-10-26 21:10:55 -0300

    Stitches e estrutura da home

---
+++ src/pages/index.tsx
@@ -0,0 +1,55 @@
+
+import { HomeContainer, Product } from "../styles/home"
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
+import Image from "next/image"
+
+export default function Home() {
+  return (
+
+    <HomeContainer>
+      <Product>
+        <Image
+          src={camiseta1}
+          width={520}
+          height={480}
+          alt="" />
+        <footer>
+          <strong>
+            Camiseta X
+          </strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image
+          src={camiseta2}
+          width={520}
+          height={480}
+          alt="" />
+        <footer>
+          <strong>
+            Camiseta X
+          </strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image
+          src={camiseta3}
+          width={520}
+          height={480}
+          alt="" />
+        <footer>
+          <strong>
+            Camiseta X
+          </strong>
+          <span>R$ 79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+
+  )
+}
